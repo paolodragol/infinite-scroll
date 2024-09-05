43bdecd7aaad20e16c1a3ba93042efa14dbeecd0
Paolo Drago Leon s296101 <s296101@studenti.polito.it>
2024-09-05 11:01:17 +0200

    Add basic functionalities and config setup

---
+++ script.js
@@ -0,0 +1,5 @@
+// Unsplash API
+const numImgs = 10;
+const apiKey = '';
+const collection = 'travel';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${numImgs}`;
